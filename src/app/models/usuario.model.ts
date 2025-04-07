64120a85d1c9ae3c532383a6bc671abd6c1bec66
jesusito <j.rios.g561@edu.utc.mx>
2025-04-07 15:23:14 -0600

    primer avance

---
+++ src/app/models/usuario.model.ts
@@ -0,0 +1,6 @@
+export class UsuarioModel {
+    //estas propiedades me ayudaran para que los datos que meta en los campos, esos valores se pasen a estas propiedades (las de abajo) y asi sea mas facil mandarlas al backend
+    email: string;
+    password: string;
+    nombre: string;
+}
